0be09e37318451d6c74cfa174ff251fe998703ad
AbdellatifOsama <abdellatifosama81@gmail.com>
2025-02-04 03:32:08 +0200

    refactor: run from main

--- main.py
+++ main.py
@@ -1,60 +1,16 @@
-from models.responses import STTResponse
-from models.requests import TTSRequest
-from services import TTS, STT
-import uuid
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse,JSONResponse
+from dotenv import load_dotenv
 import os
-from fastapi import UploadFile, File
+import uvicorn
 
-app = FastAPI(title="Text-to-Speech and Speech-to-Text API")
-
-
-
-@app.post("/generate-speech/")
-async def generate_speech(request: TTSRequest):
-    """
-    Generate an audio file from input text using Google Text-to-Speech
-    """
-    filepath, filename = TTS.generate_speech_file(
-        text=request.text, 
-        language=request.language, 
-        slow=request.slow
-    )
-    
-    return FileResponse(
-        filepath, 
-        media_type="audio/mpeg", 
-        filename=filename,
-        headers={"Content-Disposition": f"attachment; filename={filename}"}
-    )
+load_dotenv()
 
 
-    
-@app.post("/transcribe-speech/")
-async def recognize_speech(file: UploadFile = File(...)):
-    # Save the uploaded file to disk temporarily
-    temp_file_path = f"temp_{file.filename}"
-    with open(temp_file_path, "wb") as temp_file:
-        temp_file.write(await file.read())
-
-    # Use the class to recognize speech from the file
-    speech_recognition_app = STT.SpeechRecognitionApp()
-    try:
-        transcript = speech_recognition_app.recognize_speech_from_file(temp_file_path)
-        os.remove(temp_file_path)  # Clean up temporary file
-        if transcript:
-            res = STTResponse(transcript)
-            return res
-        else:
-            return JSONResponse(content={"error": "Could not understand audio."}, status_code=400)
-    except Exception as e:
-        os.remove(temp_file_path)  # Clean up temporary file
-        return JSONResponse(content={"error": str(e)}, status_code=500)
-
-    
-    
 
 
-    
+PORT = int(os.getenv("PORT", 8000))
 
+HOST =  "0.0.0.0"
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host=HOST, port=PORT, reload=True)
